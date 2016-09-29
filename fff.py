f8991acb5f3f9a884e725836ffc6d4b46e1709ca
wjohnson133 <williamleonardjohnson@gmail.com>
2016-09-29 13:00:34 -0400

    Initial commit

---
+++ fff.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# import openpyxl
+import requests
+from lxml import html
+
+
+page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+tree = html.fromstring(page, base_url="http://econpy.pythonanywhere.com/ex/001.html")
+print tree
+
